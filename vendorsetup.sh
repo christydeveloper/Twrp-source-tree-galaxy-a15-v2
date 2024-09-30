788a0b8c387b66300348d4531ff2635b23e170cf
christydeveloper <64496053+christydeveloper@users.noreply.github.com>
2024-09-30 11:42:03 -0600

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a15-user
-add_lunch_combo omni_a15-userdebug
-add_lunch_combo omni_a15-eng
+add_lunch_combo twrp_a15-user
+add_lunch_combo twrp_a15-userdebug
+add_lunch_combo twrp_a15-eng
