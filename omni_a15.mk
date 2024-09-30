6fcfbe0ecbbb6bac4211de953fd788f5942450aa
christydeveloper <64496053+christydeveloper@users.noreply.github.com>
2024-09-30 11:00:39 -0600

    Update omni_a15.mk

--- omni_a15.mk
+++ omni_a15.mk
@@ -1,29 +1,27 @@
 #
 # Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from a15 device
 $(call inherit-product, device/samsung/a15/device.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a15/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := a15
-PRODUCT_NAME := omni_a15
+PRODUCT_NAME := twrp_a15
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A155M
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="a15nsxx-user 12 SP1A.210812.016 A155FXXS3BXF1 release-keys"
-
-BUILD_FINGERPRINT := samsung/a15nsxx/a15:12/SP1A.210812.016/A155FXXS3BXF1:user/release-keys
